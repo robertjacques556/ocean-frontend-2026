db8aa14405ceafedc8229023c05d019400055b03
robertjacques556 <robertjacques556@gmail.com>
2026-01-28 01:01:20 +0000

    Iniciando o projeto Ricky and Morty

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,19 +1,27 @@
-
-
 export function Welcome() {
-
-  const titulo = "Hello Ocean"
-  const resultado =10+5;
   return (
-
-    <div>
-
-      <h1>
-        {titulo}
-      </h1>
-
-    </div> 
-    
-      
-   );
+    <>
+      <div className="header">
+        <h1>
+          Rick and Morty
+        </h1>
+      </div>
+      <div className="personagens">
+        <div className="personagem">
+          <img 
+            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" 
+          />
+          <p>
+            Rick Sanchez
+          </p>
+          <p>
+            Human
+          </p>
+          <span>
+            Alive
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
